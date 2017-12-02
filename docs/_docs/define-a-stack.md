e604268914c26031b40c69a1410c0b0e5b0c25ef
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2017-12-02 13:22:23 +0100

    Add extra parameters/tags/etc

--- docs/_docs/define-a-stack.md
+++ docs/_docs/define-a-stack.md
@@ -3,7 +3,7 @@ title: Define a Stack
 permalink: /docs/define-a-stack/
 ---
 
-Create a directory under your `stack` directory (eg: `random`)
+Create a directory under your `stack` directory (eg: `random_stack`)
 and create your template there. `cli.rb` will be loaded.
 
 ```ruby
@@ -17,6 +17,22 @@ module Stack
       client: 'cheppers',
       application: 'random'
     }
+    set_attr :capabilities, ['CAPABILITY_IAM']
+    set_attr :tags, {
+      my_extra: 'my extra tag'
+    }
+    set_attr :parameters, {
+      SSHKeyName: 'random-one'
+    }
+    set_attr :deletable, true
   end
 end
 ```
+
+Define your CloudFormation template in the `template.yaml` file
+under the same directory.
+
+**[Soon, I will update this as well]**
+
+If you want to collaborate on documentation, you can find
+the [relevant ticket here](https://github.com/Yitsushi/shelter/issues/4).
--- lib/cli/stack/cloud_formation.rb
+++ lib/cli/stack/cloud_formation.rb
@@ -14,38 +14,51 @@ module Shelter
             stack_name: get_attr(:stack_name)
           ).stacks.first
 
-          puts "#{stack.stack_name} exists | #{stack.creation_time}"
-
           cf_client.describe_stack_resources(
             stack_name: stack.stack_name
           ).stack_resources.each { |r| display_stack_resource(r) }
+        rescue Aws::CloudFormation::Errors::ValidationError
+          puts "#{get_attr(:stack_name)} does not exist"
+        end
+
+        desc 'output', 'Display resource output'
+        def output
+          stack = cf_client.describe_stacks(
+            stack_name: get_attr(:stack_name)
+          ).stacks.first
+
+          stack.outputs.each { |out| display_stack_output(out) }
         end
 
         desc 'create', 'Create stack'
         def create
           cf_client.create_stack(
             stack_name: get_attr(:stack_name),
+            capabilities: get_attr(:capabilities),
             template_body: File.open(get_attr(:template_file)).read,
-            capabilities: ['CAPABILITY_IAM'],
-            tags: stack_meta
+            tags: meta, parameters: stack_params(get_attr(:parameters))
           )
-          cf_client.wait_until(
-            :stack_create_complete,
-            stack_name: get_attr(:stack_name)
-          )
+          wait_until(:stack_create_complete, get_attr(:stack_name))
         end
 
         desc 'update', 'Update stack'
         def update
           cf_client.update_stack(
             stack_name: get_attr(:stack_name),
+            capabilities: get_attr(:capabilities),
             template_body: File.open(get_attr(:template_file)).read,
-            capabilities: ['CAPABILITY_IAM']
+            tags: meta, parameters: stack_params(get_attr(:parameters))
           )
-          cf_client.wait_until(
-            :stack_update_complete,
-            stack_name: get_attr(:stack_name)
-          )
+          wait_until(:stack_update_complete, get_attr(:stack_name))
+        rescue Aws::CloudFormation::Errors::ValidationError => e
+          puts e.message
+        end
+
+        desc 'delete', 'Delete the stack'
+        def delete
+          raise 'Stack is not deletable!!!' unless get_attr(:deletable)
+          cf_client.delete_stack(stack_name: get_attr(:stack_name))
+          wait_until(:stack_delete_complete, get_attr(:stack_name))
         end
 
         # Attribute helpers
@@ -53,7 +66,10 @@ module Shelter
           class << self
             attr_accessor :_attr
             def set_attr(name, value)
-              self._attr ||= {}
+              self._attr ||= {
+                tags: {},
+                parameters: {}
+              }
               self._attr[name] = value
             end
           end
@@ -65,28 +81,16 @@ module Shelter
 
         # AWS helpers
         no_commands do
-          def cf_client
-            @cf_client ||= Aws::CloudFormation::Client.new(
-              credentials: Aws::Credentials.new(
-                ENV.fetch('AWS_ACCESS_KEY_ID'),
-                ENV.fetch('AWS_SECRET_ACCESS_KEY')
-              )
+          include Shelter::CLI::Helpers::CloudFormation
+
+          def meta
+            stack_meta(
+              get_attr(:tags).merge(
+                client: get_attr(:meta)[:client],
+                application: get_attr(:meta)[:application]
+              ),
+              type: get_attr(:meta)[:type]
             )
-          end
-
-          def display_stack_resource(r)
-            puts "Stack Name: #{r.stack_name}"
-            puts "Resource ID: #{r.physical_resource_id}"
-            puts "Resource Type: #{r.resource_type}"
-            puts "Resource Status: #{r.resource_status}"
-          end
-
-          def stack_meta
-            [
-              { key: 'client', value: get_attr(:meta)[:client] },
-              { key: 'type', value: get_attr(:meta)[:type] },
-              { key: 'application', value: get_attr(:meta)[:application] }
-            ]
           end
         end
       end
